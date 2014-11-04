e7663aa06545f11beb393d2185db6f00dace4b11
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-04 13:53:42 +0000

    setup grid with 100 cells (all the same!)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,10 +17,26 @@ describe Board do
     end
 
     it 'has a cell stored at grid ref a1' do
-      # puts board.cells['a1']
       expect(board.cells['a1'].is_a?(Cell)).to be true
     end
 
+    it "knows about an array which contains 'a1','b10','g8' & 'j10'" do
+      expect(board.grid_ref_array).to include('a1','b10','g8','j10')
+    end
+
+    it 'grid ref array has 100 elements' do
+      expect(board.grid_ref_array.size).to eq(100)
+    end
+
+    it 'has a cell stored at several grid refs' do
+      expect(board.cells['a1'].is_a?(Cell)).to be true
+      expect(board.cells['c5'].is_a?(Cell)).to be true
+      expect(board.cells['d4'].is_a?(Cell)).to be true
+    end
+
+    it 'cells hash must include 100 elements' do
+      expect(board.cells.size).to eq(100)
+    end
 
   end
 end
